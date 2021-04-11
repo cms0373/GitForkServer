003f3f8d3599a7de6bb14bd4ead6cc4118dee931
MS <mscho0804@naver.com>
2021-04-11 22:20:25 +0900

    Hello world in master branch

--- untitled/main.py
+++ untitled/main.py
@@ -9,5 +9,8 @@ print("Hello world2")
 # adding new item in hotfix
 print("Hello world3")
 
-# adding new item in hotfix
+# adding new item in master
 print("Hello world4")
+
+# adding new item in hotfix
+print("Hello world5")
